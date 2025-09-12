92f049bf9529aa085e894e61364321c4c147a841
Gabriel Ribeiro Dos Santos <gabriel.frajola123@gmail.com>
2025-09-12 12:21:15 -0300

    Main principal (dentro do package que importa animais)

---
+++ Java-POO/Herança/pacotes/Heranca.java
@@ -0,0 +1,26 @@
+
+// Importa as classes do pacote "animais".
+import animais.Animal;
+import animais.Cachorro;
+import animais.Gato;
+
+// Classe principal com o método main
+public class Heranca {
+    public static void main(String[] args) {
+        // Criando um objeto da superclasse Animal.
+        Animal a = new Animal();
+        a.emitirSom(); // Saída: O animal faz um som.
+
+        // Criando um objeto da subclasse Cachorro.
+        Cachorro c = new Cachorro();
+        c.nome = "Neguinha"; // Acessando o atributo herdado.
+        c.emitirSom(); // Saída: O cachorro late: Au Au !!.
+        System.out.println("- Nome do Cachorro: " + c.nome);
+
+        // Criando um objeto da subclasse Gato.
+        Gato g = new Gato();
+        g.nome = "Zoe"; // Acessando atributo herdado.
+        g.emitirSom(); // Saída: O gato mia: Miauu!!.
+        System.out.println("- Nome do Gato: " + g.nome);
+    }
+}
