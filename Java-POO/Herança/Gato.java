9ff2695c20926c2750f64ebbb139a34db3ccc279
Gabriel Ribeiro Dos Santos <gabriel.frajola123@gmail.com>
2025-09-15 09:37:01 -0300

    Subclasse Gato (sem package) - Atualizando para receber os  novos atributos da Superclasse.

--- Java-POO/Herança/Gato.java
+++ Java-POO/Herança/Gato.java
@@ -1,4 +1,12 @@
+// Subclasse Gato herda da Superclasse Animal.
 public class Gato extends Animal {
+
+    // Construtor do gato recebendo os atributos da Superclasse.
+    public Gato(String nome, String raca, String racao, double peso) {
+        super(nome, raca, racao, peso); // Chama o construtor da Superclasse.
+    }
+
+    // Método sobrescrito para emitir som.
     @Override
     public void emitirSom() {
         System.out.println("O Gato mia: Miauu!!");
