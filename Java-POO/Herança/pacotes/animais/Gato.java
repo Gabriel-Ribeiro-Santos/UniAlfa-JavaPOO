73391c6f37384204375e458863e78bcdb1794c84
Gabriel Ribeiro Dos Santos <gabriel.frajola123@gmail.com>
2025-09-15 18:46:14 -0300

    Subclasse Gato (dentro do package animais) - Recebe novos atributos da Superclasse.

--- Java-POO/Herança/pacotes/animais/Gato.java
+++ Java-POO/Herança/pacotes/animais/Gato.java
@@ -3,6 +3,11 @@ package animais; // Define que essa classe pertence ao pacote "animais".
 // Subclasse Cachorro herda de animal
 public class Gato extends Animal {
 
+    // Contrutor de Gato recebendo atributos da Superclasse.
+    public Gato(String nome, String raca, String racao, double peso) {
+        super(nome, raca, racao, peso); // Chama o construtor da Superclasse.
+    }
+
     // Sobrescreve o método emitirSom da superclasse.
     @Override
     public void emitirSom() {
