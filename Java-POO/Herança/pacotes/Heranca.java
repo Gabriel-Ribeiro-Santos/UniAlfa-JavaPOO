52690c205c35332d126c81dec1b9fa2fb0a50aae
Gabriel Ribeiro Dos Santos <gabriel.frajola123@gmail.com>
2025-09-15 20:10:14 -0300

    Main principal (dentro do package animais) - Importa as classes de animais.

--- Java-POO/Herança/pacotes/Heranca.java
+++ Java-POO/Herança/pacotes/Heranca.java
@@ -3,24 +3,39 @@
 import animais.Animal;
 import animais.Cachorro;
 import animais.Gato;
+import animais.Camelo;
+import animais.Cavalo;
+import animais.Tubarao;
 
 // Classe principal com o método main
 public class Heranca {
     public static void main(String[] args) {
-        // Criando um objeto da superclasse Animal.
-        Animal a = new Animal();
-        a.emitirSom(); // Saída: O animal faz um som.
+        // Criando todos os animais.
+        Animal a = new Animal("Nome Generico.", "Desconhecida.", "Desconhecida.", 0);
+        Cachorro c = new Cachorro("Neguinha.", "Cachorro.", "Premium para cachorros.", 19);
+        Gato g = new Gato("Zoe.", "Gato.", "Premium para gatos castrados.", 9);
+        Camelo cam = new Camelo("Bactriano.", "Camelo.", "Racao para camelos.", 450);
+        Cavalo cav = new Cavalo("Pe de pano.", "Cavalo.", "Capim.", 380);
+        Tubarao t = new Tubarao("Bruce.", "Tubarao-branco.", "Peixes.", 800);
 
-        // Criando um objeto da subclasse Cachorro.
-        Cachorro c = new Cachorro();
-        c.nome = "Neguinha"; // Acessando o atributo herdado.
-        c.emitirSom(); // Saída: O cachorro late: Au Au !!.
-        System.out.println("- Nome do Cachorro: " + c.nome);
+        // Chamando método auxiliar para exibir informações de cada animal.
+        imprimirDados(a);
+        imprimirDados(c);
+        imprimirDados(g);
+        imprimirDados(cam);
+        imprimirDados(cav);
+        imprimirDados(t);
+    }
 
-        // Criando um objeto da subclasse Gato.
-        Gato g = new Gato();
-        g.nome = "Zoe"; // Acessando atributo herdado.
-        g.emitirSom(); // Saída: O gato mia: Miauu!!.
-        System.out.println("- Nome do Gato: " + g.nome);
+    // Método auxiliar que recebe um animal e imprimi todas informações formatadas.
+    public static void imprimirDados(Animal animal) {
+        System.out.println("---------Classe" + animal.getClass().getSimpleName() + "--------------");
+        System.out.println("Nome: " + animal.getNome());
+        System.out.println("Raca: " + animal.getRaca());
+        System.out.print("Som: ");
+        animal.emitirSom();
+        System.out.println("Peso: " + (int) animal.getPeso() + "KG.");
+        System.out.println("Racao: " + animal.getRacao());
+        System.out.println("--------------Fim da Classe" + animal.getClass().getSimpleName() + "--------------\n");
     }
 }
