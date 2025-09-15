a5158c0ff50574012ffd0ec114373f7a81f5b1f6
Gabriel Ribeiro Dos Santos <gabriel.frajola123@gmail.com>
2025-09-15 09:31:24 -0300

    Subclasse Cachorro (sem package) - Atualizando para receber os novos atributos da superclasse.

--- Java-POO/Herança/Cachorro.java
+++ Java-POO/Herança/Cachorro.java
@@ -1,4 +1,13 @@
+// A subclasse Cachorro herda da superclasse Animal.
 public class Cachorro extends Animal {
+
+    // Construtor do cachorro recebendo os atributos da superclasse.
+    // Assim, quando criar um cachorro, você já informa o nome, raça, ração e peso.
+    public Cachorro(String nome, String raca, String racao, double peso) {
+        super(nome, raca, racao, peso); // chama o construtor da superclasse.
+    }
+
+    // Método sobrescrito para emitir o som.
     @Override
     public void emitirSom() {
         System.out.println("O cachorro late: Au Au!!");
